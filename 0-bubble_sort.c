18a0594475963272d7724ac4b4bc6d2ea3864ace
thattechgirlie <wanji.njoro1@gmail.com>
2024-02-19 22:52:20 +0300

    sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * swap_ints - swapping ints in an array
+ * @a: first to swap
+ * @b: second int to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - sorts from greatest element first
+ * @array: arrays to sort
+ * @size: represents size of array *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t g, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (g = 0; g < len - 1; g++)
+		{
+			if (array[g] > array[g + 1])
+			{
+				swap_ints(array + g, array + g + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len --;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * swap_nodes - swapping nodes in doublelinked list
+ * @h: head pointer
+ * @n1: point to node 1
+ * @n2: point to node 2
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next-.prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2-prev;
+}
+/**
+ * insertion_sort - algorithm to sort double linked list
+ * @list: list pointer
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *s, *d, *tmp;
+
+	if *=(list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	for (s = (*list)->next; s != NULL; s = tmp)
+	{
+		tmp = s->next;
+		d = s->prev;
+		while (d != NULL && d->n < s->n)
+		{
+			swap_nodes(list, &&d, s);
+			print_list((const listint_t*)*list);
+		}
+	}
+}
