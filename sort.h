404901074149cd6dcdc8ca729026e9f78f9b61c8
thattechgirlie <wanji.njoro1@gmail.com>
2024-02-19 23:08:03 +0300

    sorting algorithms

--- sort.h
+++ sort.h
@@ -6,6 +6,20 @@
 #include <unistd.h>
 #include <string.h>
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,6 +34,8 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -33,4 +49,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void sort_deck(deck_node_t **deck);
-
+#endif
