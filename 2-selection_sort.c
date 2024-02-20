387da19b0535879820a8f3e826eab4261e3224f4
thattechgirlie <wanji.njoro1@gmail.com>
2024-02-19 23:58:43 -0800

    task2

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+/**
+ * swap_ints - swap int in array
+ * @a: int 1
+ * @b: int 2
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - selection algorithm in ascending order
+ * @array: array to sort
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t g, m;
+
+	if (array == NULL || size < 2)
+		return;
+	for (g = 0; g < size - 1; g++)
+	{
+		min = array + 1;
+		for (m = g + 1; m < size; m++)
+			min = (array[m] < *min) ? (array + m) : min;
+		if ((array + g) != min)
+		{
+			swap_ints(array + g, min);
+			print_array(array, size);
+		}
+
+	}
+}
